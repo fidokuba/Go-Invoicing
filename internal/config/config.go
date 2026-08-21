50560b2d4abe1de333d6cdb1bbbe2ffff26c07e8
Kuba Fidowicz <kuba8@hotmail.co.uk>
2026-08-21 16:41:21 +0100

    Config.go setup with default values

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,11 +1,27 @@
 package config
 
+import "os"
+
 type Config struct {
-	AppName string
+	Environment string
+	DatabaseURL string
+	Port string
 }
 
 func Load() Config {
 	return Config{
-		AppName: "Go Invoicing",
+		Environment: getEnv("APP_ENV", "development"),
+		DatabaseURL: getEnv("DATABASE_URL", "localhost:5432"),
+		Port: getEnv("APP_PORT", "8080"),
 	}
+}
+
+func getEnv(key, fallback string) string {
+	value := os.Getenv(key)
+
+	if value == "" {
+		return fallback
+	}
+
+	return value
 }
